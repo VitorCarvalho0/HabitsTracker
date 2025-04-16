99dfe3441f98ceba8a92821cc1af32985af9e088
Vitor Carvalho <euvitorcarvalho0@gmail.com>
2025-04-15 21:34:55 -0300

    add menu profile

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+let perfil = document.querySelector('#photoId')
+let menuProfile = document.querySelector('#menuProfile')
+
+function openMenu() {
+
+    menuProfile.hidden = !menuProfile.hidden;
+}
+
+
+/*
+function openMenu() {
+    if menuProfile == 'Hidden' {
+        menuProfile = innerHTML = '<div id="menuProfile">'
+    }
+}
+
+*/
