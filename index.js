01ae09c7585c593335c8df56657521c088f2ae1b
Vitor Carvalho <euvitorcarvalho0@gmail.com>
2025-04-20 23:34:04 -0300

    new atualization

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 
-
-let perfil = document.querySelector('#photoId')
-let menuProfile = document.querySelector('#menuProfile')
-
 function openMenu() {
-    menuProfile.hidden = !menuProfile.hidden;
-}
-
-function openMenuHabits() {
     var menu = document.querySelector('#menuHabits')
     menu.hidden = !menu.hidden;
 }
