da829d0dc7ef965c138293d9f428dc568b9e4d3a
Vitor Carvalho <euvitorcarvalho0@gmail.com>
2025-04-23 22:27:57 -0300

    new functions

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+let newHabit = document.querySelector('#txth')
+let btnAdd = document.querySelector('#btnAdd')
+let lhabito = document.querySelector('#listHabits')
+let lista = []
 
-function openMenu() {
-    var menu = document.querySelector('#menuHabits')
-    menu.hidden = !menu.hidden;
+btnAdd.addEventListener('click', addHabit)
+
+function addHabit() {
+    if (newHabit.value.trim() === '') return
+
+    lista.push(newHabit.value)
+
+    lhabito.innerHTML = ''
+
+    for (let habit of lista) {
+        lhabito.innerHTML += `
+        <p>
+            <input type="checkbox" class="habit-check"> ${habit}
+        </p>`
+    }
+
+    newHabit.value = ''
+    updateProgressBar()
+}
+
+function updateProgressBar() {
+    const checkboxes = document.querySelectorAll('.habit-check')
+    const total = checkboxes.length
+    let checked = 0
+
+    checkboxes.forEach(cb => {
+        if (cb.checked) checked++
+        cb.addEventListener('change', updateProgressBar)
+    })
+
+    let progress = 0
+    if (total > 0) {
+        progress = Math.round((checked / total) * 100)
+    }
+
+    document.querySelector('#ProgressBar').value = progress
 }
